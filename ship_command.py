2bea5f161ae821252ca586b1dff99b6555bf2f32
sakshyampudasaini <123820304+sakshyampudasaini@users.noreply.github.com>
2025-05-24 19:26:00 +0545

    Create ship_command.py
    This Python script simulates a simple interactive ship control system where users can start and stop the ship using typed commands. It uses a loop to continuously accept input and conditional statements to track and change the ship’s state, providing feedback for each action. Designed as a beginner-friendly project, it helps practice fundamental programming concepts like user input handling, loops, and conditionals in a clear and practical way.

---
+++ ship_command.py
@@ -0,0 +1,26 @@
+command = ""
+started = False
+while True:
+    command = input(">").lower()
+    if command == "start":
+        if started:
+            print("The ship is already started")
+        else:
+            started = True
+            print("The ship starts")
+    elif command == "stop":
+        if not started:
+            print("The ship is already stopped")
+        else:
+            started = False
+            print("The ship stops")
+    elif command == "help":
+        print("""
+start = starts the ship
+stop = stops the ship
+quit = the game quits
+""")
+    elif command == "quit":
+        break
+    else:
+        print(f"Sorry, but I didn't get you.")
